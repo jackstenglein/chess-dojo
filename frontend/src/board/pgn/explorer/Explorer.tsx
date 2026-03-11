ad145c41097079c31daada6a8ad467bd73dfbf88
Kaya van Vliet <kayavanvliet@gmail.com>
2026-03-11 13:54:26 +0100

    feat(#2000): improve Repertoire Spy discoverability
    Add navbar link under Games dropdown and rename Explorer "Player" tab
    to "Repertoire Spy". Support deep-linking via ?explorer=player URL param.

--- frontend/src/board/pgn/explorer/Explorer.tsx
+++ frontend/src/board/pgn/explorer/Explorer.tsx
@@ -4,6 +4,7 @@ import { PersonSearch } from '@mui/icons-material';
 import CloudIcon from '@mui/icons-material/Cloud';
 import { TabContext } from '@mui/lab';
 import { Box, CardContent, Tab, Tabs } from '@mui/material';
+import { useSearchParams } from 'next/navigation';
 import { useCallback, useEffect, useState } from 'react';
 import { SiLichess } from 'react-icons/si';
 import { useLocalStorage } from 'usehooks-ts';
@@ -36,7 +37,19 @@ export enum ExplorerDatabaseType {
 
 const Explorer = () => {
     const [tab, setTab] = useLocalStorage(explorerTabKey, ExplorerDatabaseType.Dojo);
+    const searchParams = useSearchParams();
     const { chess } = useChess();
+
+    useEffect(() => {
+        const explorer = searchParams.get('explorer');
+        if (
+            explorer &&
+            Object.values(ExplorerDatabaseType).includes(explorer as ExplorerDatabaseType)
+        ) {
+            setTab(explorer as ExplorerDatabaseType);
+        }
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
     const [fen, setFen] = useState(chess?.fen() ?? startingPositionFen);
     const { position, request, putPosition } = usePosition(fen);
     const [minCohort, setMinCohort] = useState('');
@@ -196,7 +209,7 @@ const Explorer = () => {
                             data-testid='explorer-tab-button-lichess'
                         />
                         <Tab
-                            label='Player'
+                            label='Repertoire Spy'
                             value={ExplorerDatabaseType.Player}
                             icon={<PersonSearch />}
                             iconPosition='start'
