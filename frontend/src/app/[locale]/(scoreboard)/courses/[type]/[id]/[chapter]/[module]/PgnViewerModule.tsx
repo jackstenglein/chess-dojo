9f3013c027522dd82a9f1c949b768fa3d5f114e7
Jack Stenglein <jackstenglein@gmail.com>
2026-08-30 13:45:16 -0500

    fix: improve UI of model games/PGN selection in courses

--- frontend/src/app/[locale]/(scoreboard)/courses/[type]/[id]/[chapter]/[module]/PgnViewerModule.tsx
+++ frontend/src/app/[locale]/(scoreboard)/courses/[type]/[id]/[chapter]/[module]/PgnViewerModule.tsx
@@ -1,5 +1,6 @@
 import PgnBoard from '@/board/pgn/PgnBoard';
-import { Box, Container } from '@mui/material';
+import { Box } from '@mui/material';
+import { DefaultUnderboardTab } from 'src/board/pgn/boardTools/underboard/underboardTabs';
 import { ModuleProps } from './Module';
 
 const PgnViewerModule: React.FC<ModuleProps> = ({ module }) => {
@@ -8,52 +9,21 @@ const PgnViewerModule: React.FC<ModuleProps> = ({ module }) => {
     }
 
     return (
-        <Container
-            maxWidth={false}
-            sx={{
-                pt: 4,
-                pb: 4,
-                px: '0 !important',
-                '--gap': '16px',
-                '--site-header-margin': '60px',
-                '--player-header-height': '0px',
-                '--toc-width': '21vw',
-                '--underboard-width': '400px',
-                '--coach-width': '400px',
-                '--tools-height': '40px',
-                '--board-width': 'calc(100vw - var(--coach-width) - 60px - var(--toc-width))',
-                '--board-height':
-                    'calc(100vh - var(--navbar-height) - var(--site-header-margin) - var(--tools-height) - 8px - 2 * var(--player-header-height))',
-                '--board-size': 'calc(min(var(--board-width), var(--board-height)))',
-            }}
-        >
-            <Box
-                sx={{
-                    display: 'grid',
-                    rowGap: '16px',
-                    gridTemplateRows: {
-                        xs: 'auto auto',
-                    },
-                    gridTemplateColumns: {
-                        xs: '1fr',
-                        md: 'var(--board-size) var(--gap) var(--coach-width) auto',
-                    },
-                    gridTemplateAreas: {
-                        xs: '"pgn" "extras"',
-                        md: '"pgn pgn pgn pgn" "extras . . ."',
-                    },
-                }}
-            >
-                <PgnBoard
-                    key={module.pgns[0]}
-                    pgn={module.pgns[0]}
-                    showPlayerHeaders={false}
-                    startOrientation={module.boardOrientation}
-                    underboardTabs={[]}
-                    disableNullMoves={false}
-                />
-            </Box>
-        </Container>
+        <Box sx={{ py: 4 }}>
+            <PgnBoard
+                key={module.pgns[0]}
+                pgn={module.pgns[0]}
+                showPlayerHeaders={false}
+                startOrientation={module.boardOrientation}
+                underboardTabs={[
+                    DefaultUnderboardTab.Explorer,
+                    DefaultUnderboardTab.Share,
+                    DefaultUnderboardTab.Settings,
+                ]}
+                disableNullMoves={false}
+                initialUnderboardTab={DefaultUnderboardTab.Explorer}
+            />
+        </Box>
     );
 };
 
